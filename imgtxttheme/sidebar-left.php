629f06fc8ccf35b5051fff6af56ae12a020e5308
S Gough <mistergough@Ss-MacBook-Pro.local>
2015-08-13 16:36:29 +0200

    Remove unused folders

--- imgtxttheme/sidebar-left.php
+++
@@ -1,5 +0,0 @@
-<?php if ( is_active_sidebar( 'Left Sidebar' ) ) : ?>
-<ul id="sidebar-left<?php if ( is_active_sidebar( 'Right Sidebar' ) ) : ?>-small<?php endif; ?>">
-<?php dynamic_sidebar( 'Left Sidebar' ); ?>
-</ul>	
-<?php endif; ?>
